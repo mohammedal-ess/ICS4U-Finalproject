d1fb8f273a89608f573e2f60a4a704f6588a997f
mohammedal-ess <99287125+mohammedal-ess@users.noreply.github.com>
2022-12-22 14:41:18 +0000

    git commit "init"

--- js/game.js
+++ js/game.js
@@ -1,10 +1,28 @@
 /* global Phaser */
 
+// Copyright (c) 2022 mohammed al-essawi All rights reserved
+//
+// Created by: mohammed al-essawi
+// Created on: Dec 2022
+// This is the phaser3 configuration file
+
 const config = {
   type: Phaser.Auto,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  //set backgroundColor
+  backgroundColor: 0x5f6e7a,
+  scale:{
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
 const game = new Phaser.Game(config)
